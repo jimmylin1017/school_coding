002a675fc62a5098fa67a04c3ec6e966545dc929
jimmy <jimmylin1017@gmail.com>
2016-09-21 23:32:31 +0800

    add sp:network system programming and hw1 update sp in README.md 2016/09/21 23:33

---
+++ sp/2016sp_hw1/is_background.c
@@ -0,0 +1,27 @@
+/*
+ * is_background.c :  check for & at end
+ */
+
+#include <stdio.h>
+#include "shell.h"
+
+int is_background(char ** myArgv) {
+
+  	if (*myArgv == NULL)
+    	return 0;
+
+  	/* Look for "&" in myArgv, and process it.
+  	 *
+	 *	- Return TRUE if found.
+	 *	- Return FALSE if not found.
+	 *
+	 * Fill in code.
+	 */
+	int i = 0;
+	while (myArgv[i] != '\0')
+	{
+		if (!strcmp(myArgv[i], "&")) return TRUE;
+		i++;
+	}
+	return FALSE;
+}
---
+++ sp/2016sp_hw1/run_command.c
@@ -0,0 +1,62 @@
+/*
+ * run_command.c :    do the fork, exec stuff, call other functions
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include "shell.h"
+
+void run_command(char **myArgv) {
+    pid_t pid;
+    int stat;
+
+    /* Create a new child process.
+     * Fill in code.
+	 */
+    pid = fork();
+
+    switch (pid) {
+
+        /* Error. */
+        case -1 :
+            perror("fork");
+            exit(errno);
+
+        /* Parent. */
+        default :
+            /* Wait for child to terminate.
+             * Fill in code.
+			 */
+            if (is_background(myArgv) == FALSE)
+                waitpid(-1, &stat, 0);
+
+            /* Optional: display exit status.  (See wstat(5).)
+             * Fill in code.
+			 */
+
+            return;
+
+        /* Child. */
+        case 0 :
+            /* Run command in child process.
+             * Fill in code.
+			 */
+            if(is_background(myArgv) == TRUE)
+            {
+                int i = 0;
+                while (myArgv[i] != '\0')
+                {
+                    if (!strcmp(myArgv[i], "&")) myArgv[i] = '\0';
+                    i++;
+                }
+            }
+            execvp(myArgv[0], myArgv);
+
+            /* Handle error return from exec */
+			exit(errno);
+    }
+}
